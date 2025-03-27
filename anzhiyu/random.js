3d33e159b2df29c33961877f2cee6eecfe62d414
caicaidog-z <149380951+caicaidog-z@users.noreply.github.com>
2025-03-27 08:41:04 +0000

    加上关于我的和小空调 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/23/cb485ce36cf0ce7f67e6e7c68b0578e1/"];function toRandomPost(){
+var posts=["2025/03/23/java学习路线/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
