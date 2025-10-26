9e60026027eaa492c7529ae54f7533be9c352ba9
caicaidog-z <149380951+caicaidog-z@users.noreply.github.com>
2025-10-26 14:26:05 +0000

    打卡 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/23/java学习路线/","2025/10/19/面试经验/"];function toRandomPost(){
+var posts=["2025/10/26/Interview/面试经验/","2025/03/23/backend/java学习路线/","2025/10/24/life/打卡01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
