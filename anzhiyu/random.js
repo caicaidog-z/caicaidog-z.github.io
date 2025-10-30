48dbfa94799fd2307fd7c5f09d62b87e507bbaab
caicaidog-z <149380951+caicaidog-z@users.noreply.github.com>
2025-10-30 14:51:24 +0000

    Update 打卡02.md Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/23/backend/java学习路线/","2025/10/24/life/打卡01/","2025/10/29/Interview/面试经验/","2025/10/31/life/打卡02/"];function toRandomPost(){
+var posts=["2025/10/30/Interview/面试经验/","2025/10/24/life/打卡01/","2025/10/31/life/打卡02/","2025/03/23/backend/java学习路线/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
